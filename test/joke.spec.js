ac8450433eebe091484ab308487096489dfc6fd4
Cyriac Domini <me@cyriacdomini.com>
2017-11-04 12:14:16 +0000

    added babel transpilation to spec.js

--- test/joke.spec.js
+++ test/joke.spec.js
@@ -1,8 +1,8 @@
-require('babel-register')
-var chicken = require ('../app/joke.js').chicken
-var expect = require('chai').expect
-// import {chicken} from '../app/joke.js'
-// inport {expect} from 'chai'
+// require('babel-register')
+// var chicken = require ('../app/joke.js').chicken
+// var expect = require('chai').expect
+import {chicken} from '../app/joke.js'
+import {expect} from 'chai'
 describe('Chicken', function(){
   it('Should have something about a road', function(){
     var containsRoad = chicken().indexOf('road') >= 0
